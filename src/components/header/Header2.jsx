9ba4451eb77d691e5300e3e473cb187ca7dd8e7a
Mohamed Abowarda <mohamedwardany352@gmail.com>
2023-11-10 01:21:51 +0200

    second part in header component

--- src/components/header/Header2.jsx
+++ src/components/header/Header2.jsx
@@ -1,5 +1,183 @@
+import {
+  Badge,
+  Container,
+  IconButton,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import PersonIcon from "@mui/icons-material/Person";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import { useState } from "react";
+import { ExpandMore } from "@mui/icons-material";
+
+const Search = styled("div")(({ theme }) => ({
+  flexGrow: 0.5,
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  border: "1px solid #777",
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  minWidth: "300px",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "#777",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
+  },
+}));
+const options = ["All Categories", "CAR", "Clothes", "Electronics"];
 function Header2() {
-  return <div>Header 2</div>;
+  // @ts-ignore
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedIndex, setSelectedIndex] = useState(1);
+  const open = Boolean(anchorEl);
+
+  const handleClickListItem = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const theme = useTheme();
+  return (
+    <Container sx={{ my: 3, display: "flex", justifyContent: "space-between" }}>
+      <Stack alignItems={"center"}>
+        <ShoppingCartIcon />
+        <Typography variant="body2">E-commerce</Typography>
+      </Stack>
+      <Search
+        sx={{
+          borderRadius: "1.5rem",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ "aria-label": "search" }}
+        />
+        <div>
+          <List
+            component="nav"
+            aria-label="Device settings"
+            sx={{
+              // @ts-ignore
+              bgcolor: theme.palette.myColor.main,
+              borderTopRightRadius: "22px",
+              borderBottomRightRadius: "22px",
+              p: "0",
+            }}
+          >
+            <ListItem
+              id="lock-button"
+              aria-haspopup="listbox"
+              aria-controls="lock-menu"
+              aria-label="when device is locked"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClickListItem}
+            >
+              <ListItemText
+                className="border"
+                sx={{
+                  width: 110,
+                  textAlign: "center",
+                  "&:hover": { cursor: "pointer" },
+                }}
+                secondary={options[selectedIndex]}
+              />
+              <ExpandMore sx={{ fontSize: "1rem", color: "white" }} />
+            </ListItem>
+          </List>
+          <Menu
+            id="lock-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "lock-button",
+              role: "listbox",
+            }}
+          >
+            {options.map((option, index) => (
+              <MenuItem
+                sx={{ fontSize: "14px" }}
+                key={option}
+                selected={index === selectedIndex}
+                onClick={(event) => handleMenuItemClick(event, index)}
+              >
+                {option}
+              </MenuItem>
+            ))}
+          </Menu>
+        </div>
+      </Search>
+
+      <div>
+        <IconButton aria-label="cart" sx={{ mx: 1 }}>
+          <StyledBadge badgeContent={4} color="primary">
+            <ShoppingCartIcon />
+          </StyledBadge>
+        </IconButton>
+
+        <IconButton>
+          <PersonIcon></PersonIcon>
+        </IconButton>
+      </div>
+    </Container>
+  );
 }
 
 export default Header2;
